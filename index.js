028e65b3857e2e2853e97d908d91909a8095c10d
“Nathan <nathanjudge27@gmail.com>
2021-05-05 22:09:46 -0700

    local host added

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.CONNECTION_URI, {
 
 
 
-let allowedOrigins = ['http://localhost:4200'];
+let allowedOrigins = ['http://localhost:4200, http://localhost:8080, http://localhost:1234, http://testsite.com'];
 
 app.use("/client", express.static(path.join(__dirname, "client", "dist")));
 
